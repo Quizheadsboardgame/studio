d9cd0199f6693203f99cbacd494018b179baff39
Owen Newton <owennewton2024@gmail.com>
2026-03-15 22:46:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1706476497-1089e",
+  "appId": "1:829971171292:web:0749cb3180aa8cd285b119",
+  "apiKey": "AIzaSyB_kOm_G8q9q7Io3vgXcny73kHyh4YuEcI",
+  "authDomain": "studio-1706476497-1089e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "829971171292"
+};
