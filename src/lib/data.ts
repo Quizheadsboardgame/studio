375ca75bd35a961eb9cfa3483579243331cf6733
Owen Newton <owennewton2024@gmail.com>
2026-03-15 22:51:49 +0000

    Now add back in all cleaners and clients

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,3 +1,4 @@
+
 export type SiteStatus =
   | 'N/A'
   | 'Client happy'
@@ -25,18 +26,45 @@ export const siteStatuses: SiteStatus[] = [
 ];
 
 export const initialSites: Omit<Site, 'id'>[] = [
-    "ACCI LEVEL 6", "ANNE MCLAREN", "BARTON HOUSE", "BAY 13", "BIO-RESIPISHORY LAB LEVEL 1",
-    "BONE RESEARCH/RADIOLOGY LEVEL 4", "CEDAR", "CLINICAL SCHOOLS", "CLIFFORD ALLBUTT BUILDING - CAB",
-    "COTON HOUSE", "E7", "EAST FORVIE (IPH - INSTITUTE OF PUBLIC HELATH)", "GRANTCHESTER HOUSE",
-    "HERSCHEL SMITH BUILDING - HSB", "HLRI - HEART & LUNG BUILDING / VICTOR PHILLIP DAHDAL",
-    "IMS LEVELS 4&5", "ISLAND RESEARCH BUILDING - IRB", "JEFFREY CHEAH (CAPELLA) OFFICE",
-    "JOHN VAN GEEST - JVG", "MEDICAL GENETICS LEVEL 6", "MEDICINE LEVEL 5", "MRC EPIDEMIOLOGY LEVEL 3",
-    "MRC WATERBEACH SAMPLE STORAGE", "NERO SPACE", "NEURO SPACE", "OBS", "OLD IMS - LAB BLOCK 4",
-    "P&A - PSYCHIATRY & ANAESTHETICS LEVEL 4", "PAEDIATRICS LEVEL 8", "POST DOC", "STRAGEWAYS (SLR)",
-    "SURGERY & RHEUMATOLOGY LEVEL 6 HUB", "SURGERY LEVEL 9", "TMS F&G LEVEL 2 OFFICE SPACE",
-    "WBIC RPU BASEMENT", "WEST FORVIE", "WOLFSON BRAIN MAIN WBIC & ANNEX ON CORNER",
+    "ACCI LEVEL 6", 
+    "ANNE MCLAREN", 
+    "BARTON HOUSE", 
+    "BAY 13", 
+    "BIO-RESIPISHORY LAB LEVEL 1",
+    "BONE RESEARCH/RADIOLOGY LEVEL 4", 
+    "CEDAR", 
+    "CLINICAL SCHOOLS", 
+    "CLIFFORD ALLBUTT BUILDING - CAB",
+    "COTON HOUSE", 
+    "E7", 
+    "EAST FORVIE (IPH - INSTITUTE OF PUBLIC HELATH)", 
+    "GRANTCHESTER HOUSE",
+    "HERSCHEL SMITH BUILDING - HSB", 
+    "HLRI - HEART & LUNG BUILDING / VICTOR PHILLIP DAHDAL",
+    "IMS LEVELS 4&5", 
+    "ISLAND RESEARCH BUILDING - IRB", 
+    "JEFFREY CHEAH (CAPELLA) OFFICE",
+    "JOHN VAN GEEST - JVG", 
+    "MEDICAL GENETICS LEVEL 6", 
+    "MEDICINE LEVEL 5", 
+    "MRC EPIDEMIOLOGY LEVEL 3",
+    "MRC WATERBEACH SAMPLE STORAGE", 
+    "NERO SPACE", 
+    "NEURO SPACE", 
+    "OBS", 
+    "OLD IMS - LAB BLOCK 4",
+    "P&A - PSYCHIATRY & ANAESTHETICS LEVEL 4", 
+    "PAEDIATRICS LEVEL 8", 
+    "POST DOC", 
+    "STRAGEWAYS (SLR)",
+    "SURGERY & RHEUMATOLOGY LEVEL 6 HUB", 
+    "SURGERY LEVEL 9", 
+    "TMS F&G LEVEL 2 OFFICE SPACE",
+    "WBIC RPU BASEMENT", 
+    "WEST FORVIE", 
+    "WOLFSON BRAIN MAIN WBIC & ANNEX ON CORNER",
     "X RAY BLOCK RADIOLOGY LEVEL 5"
-].map(name => ({ name, status: 'N/A', notes: '' }));
+].sort((a, b) => a.localeCompare(b)).map(name => ({ name, status: 'N/A' as SiteStatus, notes: '' }));
 
 
 export type CleanerPerformance =
@@ -72,7 +100,7 @@ export const initialCleaners: Omit<Cleaner, 'id'>[] = [
   "Ioana Boltasiu", "Joyce Howard", "Ludegardo Cabral", "Manuel Mendonca", "Mircalla Bond", "Nolasco De Sousa",
   "Piotr Skrzypczyk", "Rhyse Howard", "Rosana Da Silva", "Salvador Da Costa", "Sergio Dos Reis", "Shakila Soloman",
   "Thomas Boltasiu", "Veronica Smintina", "Zbigniew Bajor"
-].map(name => ({ name, rating: 'N/A', notes: '' }));
+].sort((a, b) => a.localeCompare(b)).map(name => ({ name, rating: 'N/A' as CleanerPerformance, notes: '' }));
 
 
 export type ScheduleEntry = {
